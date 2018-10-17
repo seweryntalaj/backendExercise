235b806b4a5f82bd6aca9f45b36817925e25bf7f
Seweryn91 <s.talaj@gmail.com>
2018-10-17 23:10:25 +0200

    Implement Hello class

---
+++ src/main/java/Hello.java
@@ -0,0 +1,16 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Hello implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Hello World!";
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
